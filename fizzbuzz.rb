131d5c52758edea7cc16a6f8f5e49def724e3076
Patrick Lynch <pajlynch@gmail.com>
2021-03-12 04:01:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,7 +4,7 @@ def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   elsif int % 5 == 0
-    "Buzz"    
+    "Buzz"
   elsif int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
   end
